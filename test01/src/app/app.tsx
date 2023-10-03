15fb340c2a87b72ef57f43ea5e68edfe099fafac
Andrey Paramonov <mikrobka231@gmail.com>
2023-10-04 01:59:24 +0300

    fetch: added positioning of elements on the page

--- test01/src/app/app.tsx
+++ test01/src/app/app.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
 
+import s from "./app.module.scss"
+
 import { useAppDispatch, useAppSelector } from "@/app/hooks"
 import { Like } from "@/assets/icons"
 import { Button } from "@/components/ui/button"
 import { TextField } from "@/components/ui/text-field"
 import { Typography } from "@/components/ui/typography"
-import { PostsList } from "@/features/posts/posts-list"
+import { PostsList } from "@/features/posts"
 import { fetchPosts } from "@/features/posts/posts-slice"
 
 function App() {
-  const [like, setLike] = useState(Math.random())
+  const [like, setLike] = useState("like")
   const dispatch = useAppDispatch()
   const posts = useAppSelector((state) => state.posts.posts)
   const postsStatus = useAppSelector((state) => state.posts.status)
@@ -22,10 +24,27 @@ function App() {
 
   console.log(posts)
 
+  const click = () => {
+    setLike("like")
+  }
+
   return (
-    <div>
-      <Like variant={"dislike"} flipped={true} />
-      {/*<PostsList posts={posts} />*/}
+    <div className={s.content}>
+      <div className={s.title}>
+        <Typography variant={"h1"}> Блог</Typography>
+        <Typography variant={"body1"}>
+          {" "}
+          Здесь мы делимся интересными кейсами из наших проектов, пишем про IT,
+          а также переводим зарубежные статьи
+        </Typography>
+      </div>
+      <TextField
+        placeholder={"Поиск по названию статьи"}
+        className={s.textField}
+        type={"search"}
+      />
+
+      <PostsList posts={posts} />
     </div>
   )
 }
