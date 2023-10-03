f8e73791e9ac95358682b2a536ce4ea35fc9782d
Andrey Paramonov <mikrobka231@gmail.com>
2023-10-04 02:11:49 +0300

    fetch: added styles for components

--- test01/src/main.tsx
+++ test01/src/main.tsx
@@ -2,8 +2,6 @@ import "@/styles/index.scss"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/700.css"
 
-import { StrictMode } from "react"
-
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 
